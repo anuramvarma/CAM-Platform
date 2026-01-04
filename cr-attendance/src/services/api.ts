ce74b416e58dc47c05c35804b1f16277c58198ed
anuramvarma <anuramvarma811@gmail.com>
2026-01-04 17:33:40 +0530

    Updated

--- cr-attendance/src/services/api.ts
+++ cr-attendance/src/services/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:5000/api';
+const API_URL = 'https://cam-platform.onrender.com/api';
 
 const getHeaders = () => {
     const token = localStorage.getItem('token');
