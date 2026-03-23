a489b1c62b288f6f4888d67f748a333540cb224b
anuramvarma <anuramvarma811@gmail.com>
2026-03-23 08:14:17 +0530

    New Render

--- cr-attendance/src/services/api.ts
+++ cr-attendance/src/services/api.ts
@@ -1,5 +1,5 @@
 //const API_URL = 'http://localhost:5001/api';
-const API_URL = 'https://cam-platform.vercel.app/api';
+const API_URL = 'https://cam-platformv2.onrender.com/api';
 const getHeaders = () => {
     const token = localStorage.getItem('token');
     return {
