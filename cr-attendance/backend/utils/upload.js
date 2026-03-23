40e280344561977e6c8cf000108ca2e448d2994c
anuramvarma <anuramvarma811@gmail.com>
2026-03-23 08:01:20 +0530

    FIXED

--- cr-attendance/backend/utils/upload.js
+++ cr-attendance/backend/utils/upload.js
@@ -3,9 +3,10 @@ const path = require('path');
 const fs = require('fs');
 
 // Ensure uploads directory exists
-const uploadDir = 'uploads';
+const uploadDir = '/tmp/uploads';
+
 if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
+  fs.mkdirSync(uploadDir);
 }
 
 const storage = multer.diskStorage({
